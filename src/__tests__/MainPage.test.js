d9a785a1946cf2a171e96f686c02ecbcd617dfa2
benito <benideveloperr@gmail.com>
2022-05-25 13:59:29 -0300

    resolve code smell

--- src/__tests__/MainPage.test.js
+++ src/__tests__/MainPage.test.js
@@ -2,9 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import MainPage from "../components/MainPage";
 
+beforeEach(() => render(<MainPage />));
 describe("Main page mount", () => {
   it("must display the main page title", () => {
-    render(<MainPage />);
     expect(
       screen.getByRole("heading", { name: /simpson quotes/i })
     ).toBeInTheDocument();
@@ -13,7 +13,6 @@ describe("Main page mount", () => {
 
 describe("Quotes List", () => {
   it("must dispaly 3 quotes", async () => {
-    render(<MainPage></MainPage>);
     expect(await screen.findAllByRole("listitem")).toHaveLength(3);
   });
 });
